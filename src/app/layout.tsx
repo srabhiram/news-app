ea17b83b4702636914a6b3ebb76987c4cf1b2043
srabhiram <sriramoji.abhi@gmail.com>
2025-05-07 22:30:20 +0530

    removed ads scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased  text-gray-900 dark:bg-gray-900 dark:text-gray-100`}
       >
-        <script src="https://ligheechoagool.com/88/tag.min.js" data-zone="146295" async data-cfasync="false"></script>
         <Script
           id="adsense-script"
           strategy="afterInteractive"
