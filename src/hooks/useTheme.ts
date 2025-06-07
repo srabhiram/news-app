c4bf804c4baa7ac38f1e9c39ef5b7990343423d8
srabhiram <sriramoji.abhi@gmail.com>
2025-06-07 16:48:59 +0530

    fix: darkmode optimised

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,20 +1,24 @@
-'use client'
-import  { useEffect, useState } from 'react'
+'use client';
+import { useEffect, useState } from 'react';
 
 export default function useTheme() {
-     const [isDarkMode, setIsDarkMode] = useState(false);
-    
-      useEffect(() => {
-      const isDark = localStorage.getItem("theme") === "dark";
-      document.body.classList.toggle("dark", isDark);
-      setIsDarkMode(isDark);
-    }, []);
-    
-      const toggleDarkMode = () => {
-        const newTheme = isDarkMode ? "light" : "dark";
-        setIsDarkMode(!isDarkMode);
-        localStorage.setItem("theme", newTheme);
-        document.body.classList.toggle("dark");
-      };
-  return {toggleDarkMode, isDarkMode}
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    // Only run in the browser
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem('theme') === 'dark';
+    }
+    return false; // Default fallback on server
+  });
+
+  useEffect(() => {
+    document.body.classList.toggle('dark', isDarkMode);
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    const newTheme = isDarkMode ? 'light' : 'dark';
+    localStorage.setItem('theme', newTheme);
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return { toggleDarkMode, isDarkMode };
 }
