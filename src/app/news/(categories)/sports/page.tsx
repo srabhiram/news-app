3189d73bea919a8cf1357bd3fa50f58585df1a63
srabhiram <sriramoji.abhi@gmail.com>
2025-06-07 12:17:24 +0530

    fix: add metadata for new pages

--- src/app/news/(categories)/sports/page.tsx
+++ src/app/news/(categories)/sports/page.tsx
@@ -1,8 +1,15 @@
 import LatestNewsCard from '@/components/cards/latest-news-card'
 import { NewsArticle } from '@/interface/all-interfaces'
 import { getSingleNews } from '@/lib/getSingleNews'
+import { Metadata } from 'next';
 import React from 'react'
 
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "స్పోర్ట్స్ - SRS News",
+    description: "Latest News SRS News",
+  };
+}
 export default async function page() {
     const newsArticles:NewsArticle[] = await getSingleNews("sports")
   return (
--- src/app/news/(categories)/technology/page.tsx
+++ src/app/news/(categories)/technology/page.tsx
@@ -1,8 +1,15 @@
 import LatestNewsCard from "@/components/cards/latest-news-card";
 import { NewsArticle } from "@/interface/all-interfaces";
 import { getSingleNews } from "@/lib/getSingleNews";
+import { Metadata } from "next";
 import React from "react";
 
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "టెక్నాలజీ - SRS News",
+    description: "Latest News SRS News",
+  };
+}
 export default async function page() {
   const newsArticles:NewsArticle[] = await getSingleNews("technology")
   return <LatestNewsCard newsArticles={newsArticles} categoryType="technology" heading="టెక్నాలజీ"/>
