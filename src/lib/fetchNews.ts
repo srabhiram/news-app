395ba0522ea1ff2d26d086e58ab4b66edc50df7b
Abhiram <113927865+srabhiram@users.noreply.github.com>
2025-08-13 02:58:45 +0530

    Update fetchNews.ts

--- src/lib/fetchNews.ts
+++ src/lib/fetchNews.ts
@@ -2,6 +2,7 @@ export async function fetcNews(content?:boolean) {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/api/news/getNews?content=${content}`,
       {
+        cache: "force-cache",
         next:{tags:["news"]}
       
       }
